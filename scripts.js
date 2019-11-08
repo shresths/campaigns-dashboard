4aa41b66ff21fba6f257a39b8e380b6b1fafbe69
shresth27 <shresthshruti09@gmail.com>
2019-11-08 14:35:45 +0530

    filter by date

--- scripts.js
+++ scripts.js
@@ -46,20 +46,53 @@ json_data = {
     for (i in json_data["data"]){
         let data = json_data["data"][i];
         console.log(data);
-        let currentDate = data["createdOn"];
-        let campaignHtml = data["name"];
-        let viewHtml = `<p>button html</p>`;
-        let actionsHtml = `<p>actionshtml</p>`
+        let currentDate = new Date(data["createdOn"]);
+        var today = new Date();
+        let difference = today.getTime() - currentDate.getTime();
+        let daysDifference = Math.floor(difference/1000/60/60/24);
+        let day_status = '';
+
+        console.log("difference", daysDifference)
+        if(daysDifference < 0)
+        {
+            day_status = Math.abs(daysDifference) + ' Days Left'
+        }
+        else if(daysDifference === 0) {
+            day_status = 'Campaign Live'
+        }
+        else {
+            day_status = daysDifference + ' Days Ago'
+        }
+        console.log("day status", day_status);
+        let campaignHtml = `<p>${data["name"]}</p><p>${data["region"]}</p>`;
+        let viewHtml = `<span class="row"><img src="./Page 1/Dashboard/Row/Group 4/Price.png">
+                        <p>View Pricing</p></span>`;
+        let actionsHtml = `<span class="row"><div class="col s4"><img src="./Page 1/Dashboard/Row/Group 3/file.png"><p>CSV</p></div>
+                                <div class="col s4"><img src="./Page 1/Dashboard/Row/Group 2/statistics-report.png"><p>Report</p></div>
+                                <div class="col s4"><img src="./Page 1/Dashboard/Row/Group 4/Price.png"><p>Schedule Again</p></span></div>`
+        let currentDateHtml = `<p>${formatDate(currentDate)}</p><p>${day_status}</p>`
         let new_row_html = `<tr>
-            <td>${currentDate}</td>
+            <td>${currentDateHtml}</td>
             <td>${campaignHtml}</td>
             <td>${viewHtml}</td>
             <td>${actionsHtml}</td>
             </tr>`;
 
-        $("#live_datatable tbody").append(new_row_html);
-        $("#past_datatable tbody").append(new_row_html);
-        $("#upcoming_datatable tbody").append(new_row_html);
+
+        if(today.setHours(0,0,0,0) === currentDate.setHours(0,0,0,0))
+        { 
+            $("#live_datatable tbody").append(new_row_html);
+        }
+        else if(today.setHours(0,0,0,0) < currentDate.setHours(0,0,0,0)) {
+            $("#upcoming_datatable tbody").append(new_row_html);
+        }
+        else {
+            $("#past_datatable tbody").append(new_row_html);
+        }
+
+        // $("#live_datatable tbody").append(new_row_html);
+        // $("#past_datatable tbody").append(new_row_html);
+        // $("#upcoming_datatable tbody").append(new_row_html);
 
     }
 
@@ -123,18 +156,18 @@ json_data = {
     // console.log("Future dates", JSON.stringify(futureDates, null, 4));
 
 
-    // function formatDate(date) {
-    //     var monthString = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-	// 	var year = date.getFullYear();
-    //     var hours = date.getHours();
-    //     var minutes = date.getMinutes();
-    //     var ampm = hours >= 12 ? 'pm' : 'am';
-    //     hours = hours % 12;
-    //     hours = hours ? hours : 12; // the hour '0' should be '12'
-    //     minutes = minutes < 10 ? '0'+minutes : minutes;
-    //     var strTime = hours + ':' + minutes + ' ' + ampm;
-    //     return date.getDate() + " " + monthString[date.getMonth()]  + " " + year + ", " + strTime;
-    // }
+    function formatDate(date) {
+        var monthString = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+		var year = date.getFullYear();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return date.getDate() + " " + monthString[date.getMonth()]  + " " + year + ", " + strTime;
+    }
 
     function custom_sort(a, b) {
         return new Date(a.date).getTime() - new Date(b.date).getTime();
